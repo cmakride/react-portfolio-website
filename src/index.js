ad6c8842f483b0cf91e8c91388bbeec8187e9591
ChrisMak <cmakride@gmail.com>
2022-04-18 12:57:29 -0400

    Components Initialize and Favicon

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM  from "react-dom"
+import { createRoot } from 'react-dom/client'
+import App from "./App"
+import "./index.css"
+
+const container = document.querySelector("#root")
+const root = createRoot(container)
+
+root.render(<App tab = "home" />)
