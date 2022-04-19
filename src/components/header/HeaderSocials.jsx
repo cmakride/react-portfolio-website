6aa690d12c1428021ecb9845b9ef33da5770fb21
ChrisMak <cmakride@gmail.com>
2022-04-18 20:53:23 -0400

    Header Component

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaGithubSquare,FaLinkedin } from 'react-icons/fa'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header_socials'>
+      <a href="https://www.linkedin.com/in/christopher-makrides" rel='noreferrer' target='_blank'><FaLinkedin/></a>
+      <a href="https://github.com/cmakride" rel='noreferrer' target='_blank'><FaGithubSquare/></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
