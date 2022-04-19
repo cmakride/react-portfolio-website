49aab43702a033d46dc7060b93bfb14da265649f
ChrisMak <cmakride@gmail.com>
2022-04-19 12:21:14 -0400

    Nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,16 +4,18 @@ import { AiOutlineHome } from 'react-icons/ai'
 import { FaUserAstronaut } from 'react-icons/fa'
 import { RiBookLine,RiServiceLine } from 'react-icons/ri'
 import { BiMessageDetail } from 'react-icons/bi'
+import { useState } from 'react'
 
 
 const Nav = () => {
+  const [activeNav,setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#"><AiOutlineHome/></a>
-      <a href="#about"><FaUserAstronaut/></a>
-      <a href="#experience"><RiBookLine/></a>
-      <a href="#services"><RiServiceLine/></a>
-      <a href="#contact"><BiMessageDetail/></a>
+      <a href="#" onClick={()=> setActiveNav('#')} className={activeNav==='#' ? "active" : ""}><AiOutlineHome/></a>
+      <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav==='#about' ? "active" : ""}><FaUserAstronaut/></a>
+      <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav==='#experience' ? "active" : ""}><RiBookLine/></a>
+      <a href="#services" onClick={()=> setActiveNav('#services')} className={activeNav==='#services' ? "active" : ""}><RiServiceLine/></a>
+      <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav==='#contact' ? "active" : ""}><BiMessageDetail/></a>
     </nav>
   )
 }
