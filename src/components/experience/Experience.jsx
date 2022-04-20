b2aff61c6260514fa28a2304d05feafe7cb08d87
ChrisMak <cmakride@gmail.com>
2022-04-20 16:02:04 -0400

    About P

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -104,6 +104,13 @@ const Experience = () => {
             <small className='text-light'>Experienced</small>
             </div>
           </article>
+          <article className='experience__details'>
+            <BsPatchCheckFill className='experience__details-icon' />
+            <div>
+            <h4>Flask</h4>
+            <small className='text-light'>Basic</small>
+            </div>
+          </article>
           <div className="experience__content">
           </div>
           </div>
