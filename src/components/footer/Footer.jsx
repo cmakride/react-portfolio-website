58b4f1b81c0a05396d1a16ed6a0a821eab461da1
ChrisMak <cmakride@gmail.com>
2022-04-20 22:30:56 -0400

    Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './footer.css'
+import { FaGithubSquare,FaLinkedin } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div></div>
+    <footer>
+      <a href="#" className='footer__logo'>Christopher Makrides</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+    <div className="footer__socials">
+    <a href="https://www.linkedin.com/in/christopher-makrides" rel='noreferrer' target='_blank'><FaLinkedin/></a>
+      <a href="https://github.com/cmakride" rel='noreferrer' target='_blank'><FaGithubSquare/></a>
+    </div>
+    </footer>
   )
 }
 
