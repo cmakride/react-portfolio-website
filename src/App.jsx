fb763204267e78b545287b7de18f7c17aaba2598
ChrisMak <cmakride@gmail.com>
2022-05-24 21:57:44 -0400

    Color Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
@@ -6,19 +6,30 @@ import Experience from './components/experience/Experience'
 import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+import ColorBar from './components/colorbar/ColorBar'
+
 
 
 const App = () => {
+  const [theme,setTheme] = useState('dark')
+  
+  
+  function switchTheme(color) {
+    setTheme(color)
+  }
+
+
   return (
-    <>
+    <div data-theme={theme}>
     <Header/>
+    <ColorBar switchTheme={switchTheme}/>
     <Nav/>
     <Portfolio/>
     <Experience/>
     <About/>
     <Contact/>
     <Footer/>
-    </>
+    </div>
   )
 }
 
--- src/components/colorbar/ColorBar.jsx
+++ src/components/colorbar/ColorBar.jsx
@@ -1,25 +1,18 @@
 import React from 'react'
 import './colorbar.css'
-import useLocalStorage from 'use-local-storage'
 import { BsCircleFill } from 'react-icons/bs'
 
 
-const ColorBar = () => {
+const ColorBar = ({switchTheme}) => {
 
-  const [theme, setTheme ] = useLocalStorage('dark')
-  
-  const switchTheme = (color) => {
-    const newTheme = color
-    setTheme(newTheme)
-  }
 
   return (
       
     <div className='colorbar'>
-      <i><BsCircleFill/></i>
-      <i><BsCircleFill/></i>
-      <i><BsCircleFill/></i>
-      <i><BsCircleFill/></i>
+      <i onClick={() => switchTheme('dark')}><BsCircleFill/></i>
+      <i onClick={() => switchTheme('red')}><BsCircleFill/></i>
+      <i onClick={() => switchTheme('blue')}><BsCircleFill/></i>
+      <i onClick={() => switchTheme('green')}><BsCircleFill/></i>
       
     </div>
     
