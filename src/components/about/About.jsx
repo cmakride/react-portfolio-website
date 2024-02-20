4a83d0586a085304462b977ce9aaa9af80abdabd
ChrisMak <cmakride@gmail.com>
2024-02-20 10:10:12 -0500

    The given commit message does not match the diff provided. The correct commit message based on the diff would be:
    Format and clean up About.jsx component
    
    The About.jsx file in the src/components/about directory has been formatted and cleaned up for better readability and consistency. This includes reformatting the structure of the component and adding semicolons at the end of statements.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
-import './about.css'
-import ME from '../../assets/square_me.png'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/square_me.png";
 
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Get To Know</h5>
       <h2>About Me</h2>
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
             <img src={ME} alt="About" />
-
           </div>
         </div>
         <div className="about__content">
-          
-          <p>Passionate Software Developer. My background comes from an education in Physics and Mathematics. With professional experience in the fields of Oil/Gas, Hospitality, and Electrical. I strive to work alongside people who are efficient with their work, effective in completing goals, and have an overall desire to better the world. While not coding you can find me surfing, working out at a local gym, or spending time with family.  </p>
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          <p>
+            Passionate Software Developer. My background comes from an education
+            in Physics and Mathematics. With professional experience in the
+            fields of Oil/Gas, Hospitality, and Electrical. I strive to work
+            alongside people who are efficient with their work, effective in
+            completing goals, and have an overall desire to better the world.
+            While not coding you can find me surfing, working out at a local
+            gym, or spending time with family.{" "}
+          </p>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
